b757ded5212a9d768c9a9ef4b5501690614c683a
@shlyren <shlyren@163.com>
2016-04-19 13:41:03 +0800

    da

--- ONE/Classes/Home(首页)/Controller/ONEMoviePastListVc.m
+++ ONE/Classes/Home(首页)/Controller/ONEMoviePastListVc.m
@@ -9,16 +9,15 @@
 #import "ONEMoviePastListVc.h"
 #import "ONEMoreSubtotalViewController.h"
 
+
+
 @implementation ONEMoviePastListVc
-
-
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     [tableView deselectRowAtIndexPath:indexPath animated:true];
     ONEMoreSubtotalViewController *subtotalVc = [ONEMoreSubtotalViewController new];
     subtotalVc.title = subtotalVc.month = self.pastLists[indexPath.row];
     [self.navigationController pushViewController:subtotalVc animated:true];
-    
 }
 
 @end
